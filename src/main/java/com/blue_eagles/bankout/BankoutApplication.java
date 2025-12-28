3b277706aa424e9d5763f2137a2a6e0940843509
Vlad Tanase <vmtanase@gmail.com>
2025-12-28 22:44:00 +0200

    Replace with my version on this branch, despite branching from main

---
+++ src/main/java/com/blue_eagles/bankout/BankoutApplication.java
@@ -0,0 +1,13 @@
+package com.blue_eagles.bankout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankoutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blue_eagles/bankout/BankoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blue_eagles.bankout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
